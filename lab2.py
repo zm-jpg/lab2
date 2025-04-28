913c42ad2bea3403c6145ef20191903702584902
ongzongming <zooming.24@ichat.sp.edu.sg>
2025-04-28 21:07:20 +0800

    added code for avaerage calculations and to find max and min value

--- lab2.py
+++ lab2.py
@@ -1,1 +1,37 @@
-print("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to Python")
+def display_main_menu():
+    print("Enter some numbers separated by commas")
+    return
+
+def get_user_input():
+    print("enter temperature:")
+    temp= input('Enter temperature values:')
+    inputs=temp.split(",")
+    print('temp='+str(inputs))
+    thislist = [float(i) for i in inputs]
+    return thislist
+
+
+def calc_average(numberlist):
+    average=sum(numberlist)/len(numberlist)
+    print("average is:"+ str(average))
+    return
+
+def calc_max(numlist):
+    largest=max(numlist)
+    print("maximum number is:"+str(largest))
+
+def calc_min(list):
+    lowest=min(list)
+    print("minimum number is:"+str(lowest))
+    
+
+
+def main():
+    display_main_menu()
+    x= get_user_input()
+    calc_average(x)
+    calc_max(x)
+    calc_min(x)
+
+if __name__ == "__main__":
+    main()
