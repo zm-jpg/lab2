41d4846770bc7bd8c5b07602f71ff289e2afaf5a
ongzongming <zooming.24@ichat.sp.edu.sg>
2025-05-05 20:49:13 +0800

    modified with the -1,0,1

--- bmi.py
+++ bmi.py
@@ -2,7 +2,13 @@ def calculate_bmi(height, weight):
     print("height:"+str(height))
     print("weight:"+str(weight))
     bmi = weight/(height**2)
-    return bmi
+    if(bmi<18.5):
+        x=-1
+    elif(bmi>=18.5 and bmi<=25.0):
+        x=0
+    else:
+        x=1
+    return bmi,x
 
 def classify_bmi(bmi_value):
     if(bmi_value<18.5):
@@ -13,9 +19,9 @@ def classify_bmi(bmi_value):
         print("overweight")
 
 def main():
- bmi_output=calculate_bmi(1.8,80)
- print("bmi:"+str(bmi_output))
+ bmi_output,x_1=calculate_bmi(1.8,80)
+ print("bmi="+str(bmi_output),"x_1="+str(x_1))
  classify_bmi(bmi_output)
 
 if __name__ == "__main__":
- main()
+    main()
